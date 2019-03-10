1c1333c504585113c1d1fa4f4e63fee7ce93e2df
cadamini <christian.adamini@invision.de>
2019-03-10 21:09:32 +0100

    Create and improve main class RandomCsv

---
+++ random_csv.rb
@@ -0,0 +1,40 @@
+require_relative 'export_date_range'
+require_relative 'output_generator'
+
+# generate file for importer using > operator
+# ruby random_csv.rb > filename.csv
+
+class RandomCsv
+  class << self
+    def generate_output
+      start_date, end_date = ExportDateRange.for(first_argument_or_default_year)
+      output = OutputGenerator.new(
+        region: second_argument_or_default,
+        start_date: start_date,
+        end_date: end_date,
+        queue_name: ARGV[2] || 'Test',
+        opening_hours: ARGV[3] || [8, 16]
+      ).run
+    end
+
+    private
+
+    def first_argument_or_default_year
+      ARGV[0].to_i > 0 ? ARGV[0].to_i : default_year
+    end
+
+    def second_argument_or_default
+      !ARGV[1].nil? ? ARGV[1] : default_region
+    end
+
+    def default_region
+      'de'
+    end
+
+    def default_year
+      Date.today.year
+    end
+  end
+end
+
+RandomCsv.generate_output
