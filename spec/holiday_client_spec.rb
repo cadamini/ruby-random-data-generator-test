cfb43722fdf63962645f9ad3999574b470487b32
cadamini <christian.adamini@invision.de>
2019-03-11 10:30:19 +0100

    Add holiday client spec

--- spec/holiday_client_spec.rb
+++ spec/holiday_client_spec.rb
@@ -3,4 +3,17 @@ require 'holiday_client.rb'
 require 'holidays'
 
 RSpec.describe HolidayClient do
+
+  let(:client) do
+    described_class.new(
+  		Date.new(2018,1,1), Date.new(2018,12,31), 'de'
+  	)
+  end
+  it 'accepts a startdate, enddate and country_code' do
+  	expect(client).to be_a HolidayClient 
+  end
+
+  it 'gets holiday hash for the requested year' do
+	expect(client.holidays.keys.first).to eq "Neujahrstag"
+  end
 end
